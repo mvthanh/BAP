ff571ff1a7c10f2d889ec74022b69679ef74e181
thanhmai <a1mvthanh@gmail.com>
2020-02-17 13:40:48 +0700

    BAP_EXAMPLE_05: Update KNN

--- Kmeans_KNN_NBC/KnearestNeighbor.py
+++ Kmeans_KNN_NBC/KnearestNeighbor.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-import numpy as np
 from sklearn import neighbors, datasets
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -7,26 +6,27 @@ from sklearn.metrics import accuracy_score
 
 class KNearestNeighbor_Iris:
     def __init__(self):
-        iris = datasets.load_iris()
-        self.irisX = iris.data
-        self.irisY = iris.target
+        self.model = neighbors.KNeighborsClassifier(n_neighbors=5, p=2)
 
-    def split(self, test_size):
-        return train_test_split(self.irisX, self.irisY, test_size=test_size)
+    def training(self, data, label):
+        self.model.fit(data, label)
 
-    def myweight(self, distances):
-        sigma = 0.4
-        return np.exp(-distances ** 2 / sigma)
+    def test(self, data):
+        return self.model.predict(data)
 
-    def KnearestNeighbors(self, weight='uniform'):
-        model = neighbors.KNeighborsClassifier(n_neighbors=5, p=2)  # p=2 mean norm2
-        xtrain, xtest, ytrain, ytest = self.split(100)
-        model.fit(xtrain, ytrain)
-        y_pred = model.predict(xtest)
-        print("Accuracy of 5NN: {:.2f} %".format(100 * accuracy_score(ytest, y_pred)))
+    def estimate(self, data, label):
+        pred = self.test(data)
+        print("Accuracy of 5NN: {:.2f} %".format(100 * accuracy_score(label, pred)))
 
 
-iris = KNearestNeighbor_Iris()
-iris.KnearestNeighbors()
-iris.KnearestNeighbors('distance')
-iris.KnearestNeighbors(iris.myweight)
+iris = datasets.load_iris()
+data = iris.data
+label = iris.target
+
+datatrain, datatest, labeltrain, labeltest = train_test_split(data, label, test_size=130)
+
+KNNIris = KNearestNeighbor_Iris()
+KNNIris.training(datatrain, labeltrain)
+print(KNNIris.test(datatest))
+KNNIris.estimate(datatest, labeltest)
+
